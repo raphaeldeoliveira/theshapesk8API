ebf384a7d2847aa884a534ad26726fb7d1f68af9
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-06-22 18:23:57 -0300

    implementação pra buscar todos os produtos e um produto especifico (ja testado no front)

--- src/main/java/com/theshapesk8/theshapesk8API/WebConfig.java
+++ src/main/java/com/theshapesk8/theshapesk8API/WebConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
-	@Override
+	/*@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .allowedHeaders("*");
-    }
+    }*/
 
 }
--- src/main/java/com/theshapesk8/theshapesk8API/controller/MainProduct.java
+++ src/main/java/com/theshapesk8/theshapesk8API/controller/MainProduct.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +21,7 @@ import com.theshapesk8.theshapesk8API.service.ImagemProductServices;
 import com.theshapesk8.theshapesk8API.service.ProductDetailServices;
 import com.theshapesk8.theshapesk8API.service.ProductServices;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/mainProductTeste")
 public class MainProduct {
@@ -59,5 +63,42 @@ public class MainProduct {
 		List<Product> products = productService.findByProductDetailId(id);
 		return new ProductPayload(images, products, productDetail);
 	}
+	
+	// ENDPOINT PRA ADICIONAR UM PRODUTO
+	@PostMapping(consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ProductPayload create(@RequestBody ProductPayload productPayload) {
+		//return service.create(address);
+		
+		for (ImagemProduct imageProduct : productPayload.getImages()) {
+			imageProductService.create(imageProduct);
+		}
+
+		productDetailService.create(productPayload.getProductDetail());
+		
+		for (Product product : productPayload.getProduct()) {
+			productService.create(product);
+		}
+		
+		return new ProductPayload(productPayload.getImages(), productPayload.getProduct(), productPayload.getProductDetail());
+	}
+	
+	/*@PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ProductPayload create(@ModelAttribute ProductPayload productPayload) {
+        // Implementação do método create aqui
+        // Você pode acessar os dados enviados via multipart/form-data através do objeto productPayload
+        
+        for (ImagemProduct imageProduct : productPayload.getImages()) {
+            imageProductService.create(imageProduct);
+        }
+
+        productDetailService.create(productPayload.getProductDetail());
+
+        for (Product product : productPayload.getProduct()) {
+            productService.create(product);
+        }
+
+        // Retorne o objeto ProductPayload atualizado ou conforme necessário
+        return new ProductPayload(productPayload.getImages(), productPayload.getProduct(), productPayload.getProductDetail());
+    }*/
 	
 }
